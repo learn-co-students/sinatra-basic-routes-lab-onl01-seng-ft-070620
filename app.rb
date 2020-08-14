370a41e5d3f9f2f3b5a589df8115d1e86d6efcec
Joseph cohen <jojocohen57@gmail.com>
2020-08-14 20:17:05 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do 
+    "My name is Jotaro"
+  end   
+  
+  get '/hometown' do 
+    "My hometown is CedarRapids"
+  end 
+    
+  get '/favorite-song' do 
+    "My favorite song is Impala"
+  end 
 end
