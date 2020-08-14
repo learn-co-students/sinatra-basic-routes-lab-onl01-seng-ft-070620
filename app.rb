597f35496ed70392bf80b858141e63614604cf37
cTraymany <chindalatht@gmail.com>
2020-08-14 14:42:42 +0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Chindalath."
+    end
+
+    get '/hometown' do
+        "My hometown is Vientiane."
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Mr. Twin Sister's 'In the House of Yes'."
+    end
 end
