5e5e58c81346b3653e2db47d51c376f79b506c6e
JacksonReynolds <jwreynod@gmail.com>
2020-08-14 12:43:08 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Jackson Reynolds"
+    end
+
+    get '/hometown' do
+        "My hometown is Tallahassee, FL"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Sunday Bloody Sunday by U2"
+    end
 end
