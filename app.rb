fd7b319b04acbb29dcc3ecb5264c5c7d78fb1db8
Amanda_Katherine <walker.akw@gmail.com>
2020-08-16 22:14:19 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,9 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get('/name') {"My name is Amanda."}
+
+    get('/hometown') {"My hometown is Wolfeboro, NH."}
+    
+    get('/favorite-song') {"My favorite song is ...well, I guess I don't have a favorite song, but I do enjoy Gregory Alan Isakov and The Tallest Man on Earth for artists."}
 end
